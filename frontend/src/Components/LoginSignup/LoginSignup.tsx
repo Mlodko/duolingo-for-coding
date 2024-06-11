40b021fb3cf8266e70dc0ee76b9f86f22a3be1d7
Aleksander Brzykcy <abrzykcy@student.agh.edu.pl>
2024-06-11 15:33:18 +0200

    za ba wa

---
+++ frontend/src/Components/LoginSignup/LoginSignup.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './LoginSignup.css';
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+const LoginSignup=() => {
+  return (
+    <div>
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>Sign up</div>
+                <div className='underline'></div>
+            </div>
+            <div className='inputs'>
+                <div className='input'>
+                    <img src={user_icon}alt=""/>
+                    <input type="Nickname"/>
+                </div>
+                <div className='input'>
+                    <img src={email_icon} alt=""/>
+                    <input type="email"/>
+                </div>
+                <div className='input'>
+                    <img src={password_icon} alt=""/>
+                    <input type="password"/>
+                </div>
+            </div>
+            <div className='forgot-password'>Lost Password?<span>Click Here!</span></div>
+            <div className='submit-container'>
+                <div className='submit'>Sign up</div>
+                <div className='submit'>Login</div>
+            </div>
+        </div>
+
+    </div>
+  );
+}
+
+export default LoginSignup;
